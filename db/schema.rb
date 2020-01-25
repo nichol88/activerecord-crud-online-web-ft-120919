0dae278f96a1abd990311d81237b15b5f31d125f
Brian Nicholls <nichol88@gmail.com>
2020-01-25 23:57:24 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2020_01_24_044747) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
